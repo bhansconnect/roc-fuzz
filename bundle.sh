3f209ba681b39998883c27209ca8bbc7ce5ac824
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-01-23 19:01:04 -0800

    add basic bundle script

---
+++ bundle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+mkdir build
+
+# TODO: deal with multiple platforms.
+
+clang++ -g -O1 -c \
+  -fsanitize-coverage=inline-8bit-counters \
+  -fsanitize-coverage=pc-table \
+  -fsanitize-coverage=trace-cmp \
+  platform/host.cpp -o build/platform.o
+
+cp vendor/libclang_rt.fuzzer_no_main_osx.arm64.a platform/macos-arm64.a
+
+ar -r platform/macos-arm64.a build/platform.o
