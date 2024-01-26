42fd55dad759a296632d349001bec34987f9c882
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2024-01-26 14:29:47 -0800

    use zig to build for multiple platforms

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
 cd $(dirname "${BASH_SOURCE[0]}")/..
 
@@ -7,14 +7,23 @@ mkdir -p build
 
 # TODO: deal with multiple platforms.
 
-clang++ -std=c++17 -g -O1 \
-  -Wall -Wpedantic -Wsign-conversion -Wconversion -Werror -Wextra -Wno-unused-parameter \
-  -fsanitize-coverage=inline-8bit-counters \
-  -fsanitize-coverage=pc-table \
-  -fsanitize-coverage=trace-cmp \
-  -I vendor \
-  platform/host.cpp -c -o build/platform.o
+build_target() {
+  TARGET=$1
+  ROC_TARGET=$2
+  zig c++ -std=c++17 -g -O1 -target ${TARGET} \
+    -Wall -Wpedantic -Wsign-conversion -Wconversion -Werror -Wextra -Wno-unused-parameter \
+    -fsanitize-coverage=inline-8bit-counters \
+    -fsanitize-coverage=pc-table \
+    -fsanitize-coverage=trace-cmp \
+    -I vendor \
+    platform/host.cpp -c -o build/platform.${ROC_TARGET}.o
 
-cp vendor/libclang_rt.fuzzer_no_main_osx.arm64.a platform/macos-arm64.a
+  cp vendor/libclang_rt.fuzzer_no_main.${ROC_TARGET}.a platform/${ROC_TARGET}.a
 
-ar -r platform/macos-arm64.a build/platform.o
+  zig ar -r platform/${ROC_TARGET}.a build/platform.${ROC_TARGET}.o
+}
+
+build_target x86_64-macos macos-x64
+build_target aarch64-macos macos-arm64
+build_target x86_64-linux linux-x64
+build_target aarch64-linux linux-arm64
